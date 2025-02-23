fda733223e62aebd7b43b2d30dcfc004f4fa5e78
Akakuji <marionalix@me.com>
2025-02-23 11:47:53 +0700

    Mise à jour du display de .window en flex pour meilleure gestion de l'affichage

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function updateClock() {
+	const clockElement = document.getElementById('clock');
+	const now = new Date();
+	const optionsDate = { weekday: 'short', day: '2-digit', month: 'short' };
+	const optionsTime = { hour: '2-digit', minute: '2-digit' };
+
+	const date = now.toLocaleDateString('fr-FR', optionsDate);
+	const time = now.toLocaleTimeString('fr-FR', optionsTime);
+
+	clockElement.textContent = `${date} ${time}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
 document.addEventListener('DOMContentLoaded', () => {
 	const dockIcons = document.querySelectorAll('.dock-icon');
 	const mailWindow = document.getElementById('mail-window');
@@ -10,6 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			const app = icon.dataset.app;
 
 			if (app === 'mail') {
+				console.log('ouverture de l app Mail');
 				mailWindow.classList.remove('hidden');
 			}
 		});
@@ -25,35 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
 		alert('Message envoyé avec succès !');
 		mailWindow.classList.add('hidden');
 		document.querySelector(".dock-icon[data-app='mail']").classList.remove('active');
-	});
-});
-
-
-// Mise à jour de l'horloge en haut à droite
-function updateClock() {
-	const clockElement = document.getElementById('clock');
-	const now = new Date();
-	const optionsDate = { weekday: 'short', day: '2-digit', month: 'short' };
-	const optionsTime = { hour: '2-digit', minute: '2-digit' };
-	
-	const date = now.toLocaleDateString('fr-FR', optionsDate);
-	const time = now.toLocaleTimeString('fr-FR', optionsTime);
-	
-	clockElement.textContent = `${date} ${time}`; // Suppression du "à"
-}
-
-// Actualise toutes les minutes
-setInterval(updateClock, 1000);
-updateClock();
-
-// Interaction du dock
-document.addEventListener('DOMContentLoaded', () => {
-	const dockIcons = document.querySelectorAll('.dock-icon');
-
-	dockIcons.forEach(icon => {
-		icon.addEventListener('click', () => {
-			const app = icon.dataset.app;
-			console.log(`Ouverture de l'application : ${app}`);
-		});
 	});
 });
